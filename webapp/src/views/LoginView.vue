b94c065096ae973b97778afc1e7a8dd267c23878
nicolaAtti <natti@services.imolab.it>
2020-04-28 11:49:00 +0200

    Add jetbrains ignore

---
+++ webapp/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Login msg="This is a placeholder for the login page"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'login',
+  components: {
+    Login
+  }
+}
+</script>
