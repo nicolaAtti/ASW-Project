a912670dcba5516884c54b8ba1a76e893ea574ca
nicolaAtti <natti@services.imolab.it>
2020-05-02 18:52:14 +0200

    Modified layout for mobile friendly

--- webapp/src/components/Register.vue
+++ webapp/src/components/Register.vue
@@ -32,7 +32,7 @@
             <div class="button-section">
                 <input type="submit" name="Sign Up" value="Sign Up"/>
                 <span class="achi-pub">
-     <label><input type="checkbox"  v-model="achi_pub" name="pub_achi">Display your achievements publicly.</label>
+     <label><input type="checkbox"  v-model="achi_pub" name="pub_achi">Display achievements publicly.</label>
      </span>
             </div>
         </form>
@@ -40,7 +40,7 @@
 </template>
 
 <script>
-    import axios from "axios"
+    //import axios from "axios"
 
     export default {
         name: "Register",
@@ -65,8 +65,9 @@
                 this.errors = [];
                 if(!(this.password === this.confirm_pass)){
                     //Inserted passwords do not match
-                    console.log("Throw an error telling that the passwords don't match")
-                    this.errors.push("Inserted passwords do not match.")
+                    console.log("Throw an error telling that the passwords don't match");
+                    this.errors.push("Inserted passwords do not match.");
+                    window.scrollTo(0,0)
                 }else{
                     //Begin the server side validation
                     console.log("Send the data to the registration server to begin validation")
@@ -80,67 +81,63 @@
 
 <style scoped>
     .register-form{
-    width:450px;
-    padding:30px;
-    margin:40px auto;
+    width: 80%;
+    padding: 5%;
+    margin: 3% auto;
     background: #FFF;
-    border-radius: 10px;
-    -webkit-border-radius:10px;
-    -moz-border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
-    -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
-    -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+    border-radius: 5%;
+    -webkit-border-radius: 5%;
+    -moz-border-radius: 5%;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.13);
+    -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.13);
+    -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.13);
     }
 
     .error-list b{
-        font: normal 20px 'Bitter', serif;
+        font: normal 80% 'Bitter', serif;
         color: #2A88AD;
-        margin-bottom: 5px;
+        margin-left: -13%;
+        margin-bottom: 5%;
     }
 
     .error-list ul{
         list-style: inside;
-        margin-left: -60px;
+        justify-content: center;
     }
 
     .error-list li{
-        font: normal 14px 'Bitter', serif;
+        font: normal 80% 'Bitter', serif;
         color: #ad1e1b;
-        margin-bottom: 5px;
+        margin-left: -8%;
+        margin-bottom: 5%;
     }
 
     .register-form .inner-wrap{
-        padding: 30px;
+        padding: 7%;
         background: #F8F8F8;
-        border-radius: 6px;
-        margin-bottom: 15px;
+        border-radius: 10%;
+        margin-bottom: 5%;
     }
     .register-form h1{
         background: #2A88AD;
-        padding: 20px 30px 15px 30px;
-        margin: -30px -30px 30px -30px;
-        border-radius: 10px 10px 0 0;
-        -webkit-border-radius: 10px 10px 0 0;
-        -moz-border-radius: 10px 10px 0 0;
+        padding: 2% 12% 2% 12%;
+        margin: -2% -1% 10% -1%;
         color: #fff;
         text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
-        font: normal 30px 'Bitter', serif;
-        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
-        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
-        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        font: normal 150% 'Bitter', serif;
+        -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
+        -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
+        box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
         border: 1px solid #257C9E;
     }
-    .register-form h1 > span{
-        display: block;
-        margin-top: 2px;
-        font: 13px Bitter, serif;
-    }
+
     .register-form label{
         display: block;
-        font: 13px  Bitter, serif;
+        font: 75%  Bitter, serif;
         color: #888;
-        margin-bottom: 15px;
+        margin-bottom: 5%;
     }
+
     .register-form input[type="text"],
     .register-form input[type="date"],
     .register-form input[type="datetime"],
@@ -157,72 +154,71 @@
         -webkit-box-sizing: border-box;
         -moz-box-sizing: border-box;
         width: 100%;
-        padding: 8px;
-        border-radius: 6px;
+        padding: 1%;
+        border-radius: 1%;
         -webkit-border-radius:6px;
         -moz-border-radius:6px;
         border: 2px solid #fff;
         box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
-        -moz-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
-        -webkit-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+        -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.33);
+        -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.33);
     }
 
     .gender label{
         display: inline-block;
-        padding: 20px 30px 20px 30px;
-        margin: -10px -15px 10px -15px;
+        padding: 2% 5% 2% 5%;
+        margin: -2% -5% 2% -5%;
     }
 
     .register-form .section{
-        font: normal 20px 'Bitter', serif;
+        font: normal 140% 'Bitter', serif;
         color: #2A88AD;
         margin-bottom: 5px;
     }
+
     .register-form .section span {
         background: #2A88AD;
-        padding: 5px 10px 5px 10px;
+        padding: 1% 2% 1% 2%;
         position: absolute;
         border-radius: 50%;
         -webkit-border-radius: 50%;
         -moz-border-radius: 50%;
         border: 4px solid #fff;
-        font-size: 14px;
-        margin-left: -45px;
+        font-size: 75%;
+        margin-left: -10%;
         color: #fff;
-        margin-top: -3px;
+        margin-top: -1%;
     }
 
     .register-form input[type="button"],
     .register-form input[type="submit"]{
         background: #2A88AD;
-        padding: 8px 20px 8px 20px;
-        border-radius: 5px;
-        -webkit-border-radius: 5px;
-        -moz-border-radius: 5px;
+        padding: 2% 4% 2% 4%;
+        border-radius: 10%;
+        -webkit-border-radius: 10%;
+        -moz-border-radius: 10%;
         color: #fff;
         text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
-        font: normal 30px 'Bitter', serif;
-        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
-        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
-        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        font: normal 90% 'Bitter', serif;
+        -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
+        -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
+        box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.17);
         border: 1px solid #257C9E;
-        font-size: 15px;
     }
     .register-form input[type="button"]:hover,
     .register-form input[type="submit"]:hover{
         background: #2A6881;
-        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
-        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
-        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+        -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.28);
+        -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.28);
+        box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, 0.28);
     }
     .register-form .achi-pub{
         float: left;
-        width: 250px;
-        font: 12px Bitter, serif;
+        width: 75%;
+        font: 80% Bitter, serif;
         color: #4D4D4D;
-        margin-right: 100px;
-        margin-top: 10px;
-        text-align: right;
+        margin-left: -8%;
+        margin-top: 1%;
     }
 
 
