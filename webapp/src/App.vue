959a192f3e4eb83d07397c8bd21d9da8772b0fd3
nicolaAtti <natti@services.imolab.it>
2020-05-03 13:37:53 +0200

    Add dynamic language chooser

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div class="lang_chooser">
+      <select v-model="$i18n.locale">
+        <option v-for="(lang, i) in languageArray" :key="`lang${i}`" :value="lang">{{lang}}</option>
+      </select>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -10,6 +15,12 @@ import router from "./router";
 
 export default {
   name: 'App',
+  
+  data() {
+  return {
+    languageArray: ['en','it']
+  };
+},
 
   beforeMount() {
     router.push('login')
@@ -18,12 +29,15 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .lang_chooser{
+
+  }
 </style>
