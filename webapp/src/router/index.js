04b67e2beed88ad06f86fa56039a792c6bd7a525
Michele Donati <michele.donati5@studio.unibo.it>
2020-05-08 00:41:02 +0200

    Add Home page support

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -18,7 +18,13 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Register
-  }
+  },
+    {
+      path: '/home',
+      name: 'HomePage',
+      component: HomePage
+    }
+
 ]
 
 const router = new VueRouter({
