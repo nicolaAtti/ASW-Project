fb3225bc5b3dd367419173a5661ac9d7d5050847
Daniele Schiavi <schiavi91@gmail.com>
2020-05-29 00:06:01 +0200

    Implement history-users request

--- users-service/routes/admin-routes.js
+++ users-service/routes/admin-routes.js
@@ -64,4 +64,47 @@ module.exports = function(app) {
         }
     });
 
+    app.get('/history-users', (req, res) => {
+        try {
+            const token = req.header('Authorization').replace('Bearer ', '');
+            const decodedJwt = jsonwebtoken.verify(token, JWT_SECRET);
+            if (decodedJwt.username === 'Admin') {
+                User.find({}, 'registrationDate', function (error, result) {
+                    if (error) {
+                        res.status(500).send({
+                            success: false,
+                            message: 'Internal server error'
+                        });
+                    } else {
+                        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                            'September', 'October', 'November', 'December'];
+                        const today = new Date();
+                        const year = today.getFullYear();
+                        const month = today.getMonth();
+                        const months = result.map(entry => {
+                            if (entry.registrationDate <= new Date(year, 1))
+                                return 0;
+                            else
+                                return entry.registrationDate.getMonth()
+                        });
+                        const returnVal = [];
+                        for (let i = 0; i <= month; i++) {
+                            returnVal.push({ month: monthNames[i], users: months.filter((value) => value <= i).length });
+                        }
+                        res.send(returnVal);
+                    }
+                })
+            } else {
+                res.status(401).send({
+                    success: false,
+                    message: 'Wrong token'
+                });
+            }
+        } catch (e) {
+            res.status(401).send({
+                success: false,
+                message: 'Invalid token'
+            });
+        }
+    });
 }
