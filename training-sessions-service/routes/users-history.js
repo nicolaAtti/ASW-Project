910b5409c61aea1da134f1f2b675b7dc8a9911bd
Daniele Schiavi <schiavi91@gmail.com>
2020-06-01 23:54:12 +0200

    Implement history steps request

--- training-sessions-service/routes/users-history.js
+++ training-sessions-service/routes/users-history.js
@@ -2,6 +2,8 @@ const jsonwebtoken = require('jsonwebtoken');
 const training = require("../models/training-data");
 const Training = training.model;
 const JWT_SECRET = process.env.JWT_SECRET;
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'];
 
 module.exports = function(app) {
     app.get('/users/:username/training_session/history-steps', (req, res) => {
@@ -10,7 +12,27 @@ module.exports = function(app) {
             const decodedJwt = jsonwebtoken.verify(token, JWT_SECRET);
             if (req.params.username === decodedJwt.username) {
                 Training.find({ username: decodedJwt.username }, function (error, result) {
-                    res.send(result);
+                    const today = new Date();
+                    const year = today.getFullYear();
+                    const month = today.getMonth();
+                    const initialReduce = [];
+                    for (let i = 0; i <= month; i++) {
+                        initialReduce.push({ month: monthNames[i], steps: 0 });
+                    }
+                    res.send(result
+                        .filter(entry => entry.startTime.getFullYear() === year)
+                        .map(entry => {
+                            return {
+                                month: entry.startTime.getMonth(),
+                                steps: entry.steps
+                            };
+                        })
+                        .reduce((obj, item) => {
+                            obj[item.month].steps += item.steps;
+                            return obj;
+                        }, initialReduce
+                        )
+                    );
                 })
             } else {
                 res.status(401).send({
