0c61a5ca8096a7ab05b8f70fb64b7111c5741278
nicolaAtti <natti@services.imolab.it>
2020-06-02 14:32:39 +0200

    Add fealty achievements

--- users-service/routes/users-routes.js
+++ users-service/routes/users-routes.js
@@ -68,17 +68,6 @@ module.exports = function(app) {
         });
     });
 
-
-    /**
-     * Header of request:
-     * Authorization = ACHIEVEMENT_TOKEN --> sarà dentro l'env di ogni servizio
-     *
-     * Body of request:
-     * achievementFileName = NewBlood
-     * achievementTitle = New Blood
-     * firebaseUserToken = token (deve essere mandato dall'utente durante le altre richieste)
-     *
-     */
     app.patch('/users/:username/achievement', (req, res) => {
         if (req.header('Authorization') === process.env.ACHIEVEMENT_TOKEN) {
             const achievementFileName = req.body.achievementFileName;
@@ -86,7 +75,56 @@ module.exports = function(app) {
             const username = req.params.username;
             //Add check for duplicate achievements -- trova un modo decente per gestire i ritorni
 
-            User.findOneAndUpdate({username: username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, awardAchievement(error,result));
+            User.findOneAndUpdate({username: username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, function (error,result) {
+                if (error) {
+                    if (error.code === 11000) {
+
+                        res.status(409).send({
+                            success: false,
+                            message: 'Username or email already present'
+                        })
+                    } else {
+                        if (error.name === "ValidationError") {
+                            res.status(400).send({
+                                success: false,
+                                message: 'Wrong parameters'
+                            })
+                        } else {
+                            res.status(500).send({
+                                success: false,
+                                message: 'Failed to save user'
+                            })
+                        }
+                    }
+                } else {
+                    if (result === null) {
+                        res.status(404).send({
+                            success: false,
+                            message: 'Username not found'
+                        });
+                    } else {
+                        const notificationPayload = {
+                            notification: {
+                                title: "Achievement Earned!",
+                                body: "Congratulations! You earned the achievement "+achievementTitle+" !",
+                                icon: "party_icon.png"
+                            },
+                            to: req.body.firebaseUserToken
+                        };
+                        axios.post("https://fcm.googleapis.com/fcm/send", notificationPayload,{headers: { Authorization: "key="+process.env.FIREBASE_SENDER_TOKEN, 'Content-Type': "application/json"}}).then(response => {
+                            res.send({
+                                success: true,
+                                message: 'Achievement successfully assigned'
+                            });
+                        }).catch(error => {
+                            res.send({
+                                success: true,
+                                message: 'Failed to award achievement:'+error
+                            })
+                        })
+                    }
+                }
+            });
         } else {
             res.status(401).send({
                 success: false,
@@ -204,17 +242,104 @@ module.exports = function(app) {
                     const daysDiff = Math.ceil(Math.abs(today - registationDate) / (1000*60*60*24));
                     if(daysDiff >= 180){
                         const achievementFileName = "AToastToUs";
-                        User.findOneAndUpdate({username: req.params.username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, awardAchievement(error,result));
+                        const achievementTitle = "A toast to us";
+                        User.findOneAndUpdate({username: req.params.username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, function(error,result){
+                            if (error) {
+                                if (error.code === 11000) {
+
+                                    res.status(409).send({
+                                        success: false,
+                                        message: 'Username or email already present'
+                                    })
+                                } else {
+                                    if (error.name === "ValidationError") {
+                                        res.status(400).send({
+                                            success: false,
+                                            message: 'Wrong parameters'
+                                        })
+                                    } else {
+                                        res.status(500).send({
+                                            success: false,
+                                            message: 'Failed to save user'
+                                        })
+                                    }
+                                }
+                            } else {
+                                if (result === null) {
+                                    res.status(404).send({
+                                        success: false,
+                                        message: 'Username not found'
+                                    });
+                                } else {
+                                    const notificationPayload = {
+                                        notification: {
+                                            title: "Achievement Earned!",
+                                            body: "Congratulations! You earned the achievement "+achievementTitle+" !",
+                                            icon: "party_icon.png"
+                                        },
+                                        to: req.body.firebaseUserToken
+                                    };
+                                    axios.post("https://fcm.googleapis.com/fcm/send", notificationPayload,{headers: { Authorization: "key="+process.env.FIREBASE_SENDER_TOKEN, 'Content-Type': "application/json"}}).then(response => {
+                                        console.log("Notification Sent" + response)
+                                    }).catch(error => {
+                                        console.log("Notification not sent" +error)
+                                    })
+                                }
+                            }
+                        });
                     }
                     if(daysDiff >= 365){
                         const achievementFileName = "AYearWithUs";
-                        User.findOneAndUpdate({username: req.params.username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, awardAchievement(error,result));
-                    }
+                        const achievementTitle = "A year with us";
+                        User.findOneAndUpdate({username: req.params.username, achievements: {$nin: [achievementFileName]}}, {$push: {achievements: achievementFileName}}, function(error,result){
+                            if (error) {
+                                if (error.code === 11000) {
 
-                    res.send({
+                                    res.status(409).send({
+                                        success: false,
+                                        message: 'Username or email already present'
+                                    })
+                                } else {
+                                    if (error.name === "ValidationError") {
+                                        res.status(400).send({
+                                            success: false,
+                                            message: 'Wrong parameters'
+                                        })
+                                    } else {
+                                        res.status(500).send({
+                                            success: false,
+                                            message: 'Failed to save user'
+                                        })
+                                    }
+                                }
+                            } else {
+                                if (result === null) {
+                                    res.status(404).send({
+                                        success: false,
+                                        message: 'Username not found'
+                                    });
+                                } else {
+                                    const notificationPayload = {
+                                        notification: {
+                                            title: "Achievement Earned!",
+                                            body: "Congratulations! You earned the achievement "+achievementTitle+" !",
+                                            icon: "party_icon.png"
+                                        },
+                                        to: req.body.firebaseUserToken
+                                    };
+                                    axios.post("https://fcm.googleapis.com/fcm/send", notificationPayload,{headers: { Authorization: "key="+process.env.FIREBASE_SENDER_TOKEN, 'Content-Type': "application/json"}}).then(response => {
+                                        console.log("Notification Sent" + response)
+                                    }).catch(error => {
+                                        console.log("Notification not Sent" + error)
+                                    })
+                                }
+                            }
+                        });
+                    }
+                    res.status(201).send({
                         success: true,
                         token: token
-                    });
+                    })
                 } else {
                     res.status(401).send({
                         success: false,
@@ -284,54 +409,4 @@ module.exports = function(app) {
             });
         }
     });
-}
-
-function awardAchievement(error,result) {
-    if (error) {
-        if (error.code === 11000) {
-            res.status(409).send({
-                success: false,
-                message: 'Username or email already present'
-            })
-        } else {
-            if (error.name === "ValidationError") {
-                res.status(400).send({
-                    success: false,
-                    message: 'Wrong parameters'
-                })
-            } else {
-                res.status(500).send({
-                    success: false,
-                    message: 'Failed to save user'
-                })
-            }
-        }
-    } else {
-        if (result === null) {
-            res.status(404).send({
-                success: false,
-                message: 'Username not found'
-            });
-        } else {
-            const notificationPayload = {
-                notification: {
-                    title: "Achievement Earned!",
-                    body: "Congratulations! You earned the achievement "+achievementTitle+" !",
-                    icon: "party_icon.png"
-                },
-                to: req.body.firebaseUserToken
-            };
-            axios.post("https://fcm.googleapis.com/fcm/send", notificationPayload,{headers: { Authorization: "key="+process.env.FIREBASE_SENDER_TOKEN, 'Content-Type': "application/json"}}).then(response => {
-                res.send({
-                    success: true,
-                    message: 'Achievement successfully assigned'
-                });
-            }).catch(error => {
-                res.send({
-                    success: true,
-                    message: 'Failed to award achievement:'+error
-                })
-            })
-        }
-    }
-}
+};
