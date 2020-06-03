db8a7d60dd982cd88b7ebef718960de912f87bbe
nicolaAtti <natti@services.imolab.it>
2020-06-03 21:49:10 +0200

    Fix firebase config

--- webapp/public/firebase-messaging-sw.js
+++ webapp/public/firebase-messaging-sw.js
@@ -101,13 +101,13 @@ messaging.setBackgroundMessageHandler(function(payload) {
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
  firebase.initializeApp({
-     apiKey: "AIzaSyCK08PeoVv-ii67bXbEq90PMlIVdhZtyDI",
-     authDomain: "ads-training-2362b.firebaseapp.com",
-     databaseURL: "https://ads-training-2362b.firebaseio.com",
-     projectId: "ads-training-2362b",
-     storageBucket: "ads-training-2362b.appspot.com",
-     messagingSenderId: "4207914346",
-     appId: "1:4207914346:web:48fa14670e11b3fd42c913"
+     apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+     databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+     projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+     storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID,
+     appId: process.env.VUE_APP_FIREBASE_APP_ID
  });
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
